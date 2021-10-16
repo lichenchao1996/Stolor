0d8a23d551775d7b9552c2ee28f9f5f4bf637f67
lichenchao1996 <lichenchao1996@gmail.com>
2021-10-16 18:47:37 +0800

    分类页文章卡片背景图

--- archive.php
+++ archive.php
@@ -23,19 +23,21 @@
                     <?php while ($this->next()) { $indexId = 'postTitle' . $index; $red = rand(1, 254); $green = rand(1, 254); $blue = rand(1, 254); ?>
                         <li class="post-item grid-item" itemscope itemtype="http://schema.org/BlogPosting">
                             <?php if ($GLOBALS['isDiffColorCard'] == 'on') : ?>
-                                <a class="post-link" style="border: 4px solid rgb(<?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>);" href="<?php $this->permalink() ?>" onmouseover="mouseOverColor(<?php echo $indexId?>, <?php echo (255 - $red) ?>, <?php echo (255 - $green) ?>, <?php echo (255 - $blue) ?>)" onmouseout="mouseOutColor(<?php echo $indexId?>, <?php echo (255 - $red) ?>, <?php echo (255 - $green) ?>, <?php echo (255 - $blue) ?>)">
+                                <a class="post-link" style="border: 4px solid rgb(<?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>);" href="<?php $this->permalink() ?>" onmouseover="mouseOverColor(<?php echo $indexId ?>, <?php echo (255 - $red) ?>, <?php echo (255 - $green) ?>, <?php echo (255 - $blue) ?>)" onmouseout="mouseOutColor(<?php echo $indexId?>, <?php echo (255 - $red) ?>, <?php echo (255 - $green) ?>, <?php echo (255 - $blue) ?>)">
+                                <h3 id="<?php echo $indexId ?>" class="post-title" style="color: rgb(<?php echo (255 - $red) ?>, <?php echo (255 - $green) ?>, <?php echo (255 - $blue) ?>);">
                             <?php else : ?>
-                                <a class="post-link" style="border: 4px solid rgb(<?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>);" href="<?php $this->permalink() ?>" onmouseover="mouseOverColor(<?php echo $indexId?>, <?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>)" onmouseout="mouseOutColor(<?php echo $indexId?>, <?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>)">
+                                <a class="post-link" style="border: 4px solid rgb(<?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>);" href="<?php $this->permalink() ?>" onmouseover="mouseOverColor(<?php echo $indexId ?>, <?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>)" onmouseout="mouseOutColor(<?php echo $indexId?>, <?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>)">
+                                <h3 id="<?php echo $indexId ?>" class="post-title" style="color: rgb(<?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>);">
                             <?php endif; ?>
-                                <?php if ($GLOBALS['isDiffColorCard'] == 'on') : ?>
-                                    <h3 id="<?php echo $indexId ?>" class="post-title" style="color: rgb(<?php echo (255 - $red) ?>, <?php echo (255 - $green) ?>, <?php echo (255 - $blue) ?>);">
-                                <?php else : ?>
-                                    <h3 id="<?php echo $indexId ?>" class="post-title" style="color: rgb(<?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>);">
-                                <?php endif; ?>
                                     <time class="index-time" datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('M j, Y'); ?></time>
                                     <br>
                                     <span><?php $this->title() ?></span>
                                 </h3>
+                                <?php
+                                    if (@$this->fields->cover) {
+                                        echo '<span class="cover" style="background: url(' . $this->fields->cover . ') center/cover no-repeat !important; opacity:' . $GLOBALS['style_BG_opacity'] . ';"></span>';
+                                    }
+                                ?>
                                 <?php if ($this->category) : ?>
                                     <div class="post-meta" style="background: rgb(<?php echo $red ?>, <?php echo $green ?>, <?php echo $blue ?>);">
                                         <?php echo $this->category(', ', false); ?>
